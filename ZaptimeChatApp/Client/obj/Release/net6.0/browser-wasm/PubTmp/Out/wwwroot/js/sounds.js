210d430db02c561c3c1fb63c73e9c4f6e7ef067a
OG-Enoch <144890721+vEnoch24@users.noreply.github.com>
2024-07-22 12:13:24 +0100

    added sql database connection string

---
+++ ZaptimeChatApp/Client/obj/Release/net6.0/browser-wasm/PubTmp/Out/wwwroot/js/sounds.js
@@ -0,0 +1,7 @@
+﻿
+function playNotificationSound() {
+    var audio = new Audio("~\\Client\\wwwroot\\js\\sounds\\notification.mp3"); // Path to sound file
+    audio.play().catch(function (error) {
+        console.log('Playback failed:', error);
+    });
+}
